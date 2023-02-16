a4607f56ade9fc746e06a249104c89e7504f1c7c
pedrorubens <pedrorubens.pp@gmail.com>
2023-02-16 15:43:48 -0300

    add: page bookshelf

--- public/index.js
+++ public/index.js
@@ -9,3 +9,4 @@ const root = document.querySelector("#root");
 const spa = SPA();
 
 root.appendChild(spa.getPage(window.location.pathname));
+
---
+++ public/modules/carrossel.js
@@ -0,0 +1,8 @@
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+const title = document.querySelector(".title_shiefbook");
+
+
+
