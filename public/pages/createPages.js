53670e15544e1ea2b2c6f079bd04ab3b8cb02166
pedrorubens <pedrorubens.pp@gmail.com>
2023-02-20 13:09:45 -0300

    add: new page

--- public/pages/createPages.js
+++ public/pages/createPages.js
@@ -1,27 +1,19 @@
 // @author {Pedro}
 
-import { isLeft, isRight } from "../modules/carrossel.js";
 import { createPage } from "../modules/createPage.js";
 
+export default function formsPage() {
 
-import loadBook from "../modules/bookLoader.js";
-// import { response } from "express";
-
-export default function bookshelf() {
     const container = document.createElement("div");
     container.classList.add('containerPages');
-    container.id = "containerPages";
-
-    const divbtn = document.createElement("div");
-    divbtn.classList.add('divbtn');
 
     const divInputConteudo = document.createElement("div");
     divInputConteudo.classList.add("input-container");
 
-    const divInputImg = document.createElement("div");
+    const divInputImg =  document.createElement("div");
     divInputImg.classList.add("input-container");
 
-    const divInputTopic = document.createElement("div");
+    const divInputTopic =  document.createElement("div");
     divInputTopic.classList.add("input-container");
 
     const title = document.createElement("h1");
@@ -71,7 +63,7 @@ export default function bookshelf() {
 
     const labelImg = document.createElement("label");
     labelImg.setAttribute("for", "nome-do-campo");
-    labelImg.textContent = "Upload de Imagem:   ";
+    labelImg.textContent = "Upload de Imagem:";
 
     const inputBtn = document.createElement("input");
     inputBtn.setAttribute("type", "submit");
@@ -97,89 +89,17 @@ export default function bookshelf() {
     divInputImg.appendChild(labelImg);
     divInputTopic.appendChild(labelTopic);
 
-    form.appendChild(divInputTopic);
     form.appendChild(divInputConteudo);
     form.appendChild(divInputImg);
+    form.appendChild(divInputTopic);
 
-
-
+    form.appendChild(inputBtn);
 
     divContainer.appendChild(form);
+    divContainer.appendChild(newPage);
+
     container.appendChild(title);
-
-    divbtn.appendChild(newPage);
-    divbtn.appendChild(inputBtn);
-
-    container.appendChild(divbtn);
-
-
     container.appendChild(divContainer);
-
 
     return container;
 };
-
-
-// export default function bookshelf() {
-//     const container = document.createElement("div");
-//     container.classList.add('container', "backgrondShilf");
-
-//     const title = document.createElement("h1");
-//     title.className = "title_shiefbook";
-
-//     const galleryWrapper = document.createElement("div");
-//     galleryWrapper.className = "gallery-wrapper";
-
-//     const gallery = document.createElement("div");
-//     gallery.className = "gallery";
-
-//     const previousImage = document.createElement("button");
-//     previousImage.type = "button";
-//     previousImage.textContent = "◀";
-//     previousImage.classList.add("arrow-left", "control");
-//     previousImage.onclick = () => isLeft();
-
-//     const nextImage = document.createElement("button");
-//     nextImage.type = "button";
-//     nextImage.textContent = "▶";
-//     nextImage.classList.add("arrow-right", "control");
-//     nextImage.onclick = () => isRight();
-
-//     const json = fetch(`http://localhost:3000/api/bookshelves`)
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((response) => {
-//             response.forEach((shelf, index) => {
-//                 console.log(shelf);
-//                 const BookShelf = document.createElement("img");
-//                 BookShelf.src = "http://localhost:3000/images/prateleira.png";
-//                 BookShelf.classList.add("item");
-//                 BookShelf.alt = shelf.id;
-//                 BookShelf.onclick = async (e) => {
-//                     console.log('teste')
-//                     await loadBook(shelf.id)
-//                 }
-//                 if (index === 0) {
-//                     BookShelf.classList.add("current-item");
-//                     title.textContent = shelf.name
-
-//                 }
-//                 BookShelf.dataset.name = shelf.name;
-//                 BookShelf.dataset.id = shelf.id;
-
-//                 gallery.appendChild(BookShelf);
-//             });
-//         });
-
-//     const header = document.createElement("div");
-//     header.classList.add("headerShelf");
-//     header.appendChild(previousImage);
-//     header.appendChild(title);
-//     header.appendChild(nextImage);
-
-//     galleryWrapper.appendChild(gallery);
-//     container.appendChild(header);
-//     container.appendChild(galleryWrapper);
-//     return container;
-// }
