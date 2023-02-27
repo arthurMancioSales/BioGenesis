e1161991011e64a270c5dc27d328d7860a1d1375
Davi Severino <daviseverino96@gmail.com>
2023-02-27 19:09:37 -0300

    git to pull

--- public/pages/aboutUs.js
+++ public/pages/aboutUs.js
@@ -8,61 +8,66 @@ export default function aboutUs() {
     const divInitial = document.createElement("div");
     divInitial.classList.add("aboutUsBg", "bodyHome");
 
-    // cria o cabeçalho
     const header = document.createElement('header');
-    header.classList.add('flexRowSpace');
+    header.classList.add('flexColumn', "mainSectionList");
 
-    // cria a primeira div do cabeçalho
-    const div1 = document.createElement('div');
-    div1.classList.add('flexRowSpaceEven', 'boxSize');
+    const title = document.createElement('h1');
+    title.classList.add('listTitle');
+    title.textContent = 'SOBRE NÓS';
 
-    header.appendChild(div1);
-
-    divInitial.appendChild(header); 
-
-    // cria o conteúdo principal
+    header.appendChild(title);
+ 
     const main = document.createElement('main');
-    main.classList.add('flexRowSpaceEven', 'mainSize');
+    main.classList.add('flexColumn', 'mainSize');
 
-    // cria a seção principal
     const section = document.createElement('section');
-    section.classList.add('sectionBox', "flexColumn");
+    section.classList.add('sectionBox', "flexRowSpaceAround");
 
-    // cria o título da seção
-    const h1Section = document.createElement('h1');
-    h1Section.classList.add('titleText');
-    h1Section.textContent = 'SOBRE NÓS';
-    section.appendChild(h1Section);
+    const creator1 = document.createElement('div');
+    creator1.classList.add('flexColumn');
 
-    // cria o parágrafo da seção
-    const pSection = document.createElement('p');
-    pSection.classList.add('bodyText');
-    pSection.textContent = 'Lorem ipsum dolor sit amet consectetur. Aenean eget nulla ac elementum non tellus risus. Ullamcorper volutpat aliquam neque mauris turpis interdum dolor. Nulla turpis porttitor magna tortor etiam nunc sed dis vitae.';
-    section.appendChild(pSection);
+    const img = document.createElement("img");
+    img.setAttribute('src', '/images/logo.png');
+    img.style.width = "150px"
 
-    // cria o botão da seção
-    const buttonSection = document.createElement('button');
-    buttonSection.setAttribute('type', 'button');
-    buttonSection.setAttribute('class', 'button');
-    buttonSection.textContent = 'COMEÇAR';
-    buttonSection.onclick = () => spa.redirect("/bookshelves");
+    const pName = document.createElement("p");
+    pName.textContent = "NOME"
+    pName.classList.add('bodyText');
 
-    section.appendChild(buttonSection);
+    const pAge = document.createElement("p");
+    pAge.textContent = "XX anos"
+    pAge.classList.add('bodyText');
+
+    const pFunction = document.createElement("p");
+    pFunction.textContent = "Desenvolvedor Back-End"
+    pFunction.classList.add('bodyText');
+
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.setAttribute('class', 'confirmDeleteButton');
+    button.textContent = 'Contato';
+    button.style.width = "150px"
+    button.style.height = "50px"
+    //button.onclick = () => spa.redirect("/bookshelves");
+
+    creator1.appendChild(img)
+    creator1.appendChild(pName)
+    creator1.appendChild(pAge)
+    creator1.appendChild(pFunction)
+    creator1.appendChild(button)
+    /* const creator2 = document.createElement('div');
+
+    const creator3 = document.createElement('div');
+
+    const creator4 = document.createElement('div'); */
+
+    section.appendChild(creator1);
+    /* section.appendChild(creator2);
+    section.appendChild(creator3);
+    section.appendChild(creator4); */
+
+   // main.appendChild(title);
     main.appendChild(section);
-
-    // cria o aside
-    const aside = document.createElement('aside');
-    aside.classList.add('homeAside');
-
-    // cria a imagem do aside
-    const imgAside = document.createElement('img');
-    imgAside.classList.add('link');
-    imgAside.setAttribute('src', '/images/estante.png');
-    imgAside.setAttribute('id', 'shelf');
-    imgAside.onclick = () => spa.redirect("/bookshelves");
-
-    aside.appendChild(imgAside);
-    main.appendChild(aside);
 
     // adiciona tudo ao body da página
     divInitial.appendChild(header);
