3724b96acc67d5900994d9fe9518547754aa093f
arthurMancioSales <arthur_mancio@outlook.com>
2023-03-02 18:33:06 -0300

    Add: redirecionamento porta 80 para 443

--- app.js
+++ app.js
@@ -6,32 +6,38 @@ import dotenv from "dotenv";
 import router from "./src/router.js";
 import cookieParser from "cookie-parser";
 
-// Carrega variáveis de ambiente
+// Load enviroment variables
 dotenv.config();
 
-// Instancia aplicação express
+// Start express application
 const app = express();
 const port = process.env.SERVER_PORT;
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+
+// Routes
+app.use("/", express.static("./public"));
+app.use("/", router);
+
+app.use((req, res, next) => {
+    if (req.headers["x-forwarded-proto"] !== "https") {
+        res.redirect("https://" + req.hostname + req.url);
+    }
+});
+
+// 404 redirect
+app.use(function (req, res) {
+    res.status(404).send("Não foi possível encontrar o recurso especificado");
+});
 
 const options = {
     key: fs.readFileSync("server.key"),
     cert: fs.readFileSync("server.cert"),
 };
 
-// Middlewares
-app.use(express.json());
-app.use(cookieParser());
-
-// Rotas
-app.use("/", express.static("./public"));
-app.use("/", router);
-
-// Redirecionamento de página não encontrada
-app.use(function (req, res) {
-    res.status(404).send("Não foi possível encontrar o recurso especificado");
-});
-
-//  Roda o servidor
+// Start the server
 const server = https.createServer(options, app);
 server.listen(port, () => {
     console.log(`Server running on https://149.28.100.51:${port}`);
